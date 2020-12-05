caf60e0caf8ecacada7e04f7fbb98dd6d6df57f7
dong <945917883@qq.com>
2020-12-05 14:47:33 +0800

    jwt 工具类 和 uuid工具类

--- miaosha-common/src/main/java/com/dong/common/jwt/JwtTokenUtil.java
+++ miaosha-common/src/main/java/com/dong/common/jwt/JwtTokenUtil.java
@@ -1,125 +1,1 @@
-package com.dong.common.jwt;
-
-import cn.hutool.core.util.RandomUtil;
-import io.jsonwebtoken.*;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-
-/**
- * @author dong
- */
-@Component
-public class JwtTokenUtil {
-    @Autowired
-    private JwtProperties jwtProperties;
-
-    /**
-     * 生成token(通过用户名和签名时候用的随机数)
-     * @param userName
-     * @param randomKey
-     * @return
-     */
-    public String generateToken(String userName,String randomKey){
-        Map<String,Object> claims = new HashMap<>();
-        claims.put(jwtProperties.getMd5Key(),randomKey);
-        return doGenerateToken(claims,userName);
-    }
-
-    /**
-     * 生成token
-     * @param claims
-     * @param subject
-     * @return
-     */
-    private String doGenerateToken(Map<String,Object> claims,String subject ){
-        final Date createDate = new Date();
-        final Date expirationDate = new Date(createDate.getTime()+jwtProperties.getExpiration()*1000);
-        return Jwts.builder()
-                .setClaims(claims)
-                .setSubject(subject)
-                .setIssuedAt(createDate)
-                .setExpiration(expirationDate)
-                .signWith(SignatureAlgorithm.HS512,jwtProperties.getSecret())
-                .compact();
-    }
-
-    /**
-     * 获取混淆MD5签名用的随机字符串
-     * @return
-     */
-    public String getRandomKey(){
-        return RandomUtil.randomString(6);
-    }
-    /**
-     * <pre>
-     *  验证token是否失效
-     *  true:过期   false:没过期
-     * </pre>
-     */
-    public Boolean isTokenExpired(String token) {
-        try {
-            final Date expiration = getExpirationDateFromToken(token);
-            return expiration.before(new Date());
-        } catch (ExpiredJwtException expiredJwtException) {
-            return true;
-        }
-    }
-
-    /**
-     * 获取jwt的payload部分
-     * @param token
-     * @return
-     */
-    private Claims getClaimFromToken(String token) {
-        return Jwts.parser()
-                .setSigningKey(jwtProperties.getSecret())
-                .parseClaimsJws(token)
-                .getBody();
-    }
-    /**
-     * 获取用户名从token中
-     */
-    public String getUsernameFromToken(String token) {
-        return getClaimFromToken(token).getSubject();
-    }
-
-    /**
-     * 获取jwt发布时间
-     */
-    public Date getIssuedAtDateFromToken(String token) {
-        return getClaimFromToken(token).getIssuedAt();
-    }
-
-    /**
-     * 获取jwt失效时间
-     */
-    public Date getExpirationDateFromToken(String token) {
-        return getClaimFromToken(token).getExpiration();
-    }
-    /**
-     * 解析token是否正确,不正确会报异常<br>
-     */
-    public void parseToken(String token) throws JwtException {
-        Jwts.parser().setSigningKey(jwtProperties.getSecret()).parseClaimsJws(token).getBody();
-    }
-    /**
-     * 获取私有的jwt claim
-     */
-    public String getPrivateClaimFromToken(String token, String key) {
-        return getClaimFromToken(token).get(key).toString();
-    }
-
-    /**
-     * 获取md5 key从token中
-     */
-    public String getMd5KeyFromToken(String token) {
-        return getPrivateClaimFromToken(token, jwtProperties.getMd5Key());
-    }
-
-
-
-}
+package com.dong.common.jwt;import cn.hutool.core.util.RandomUtil;import io.jsonwebtoken.*;import org.springframework.beans.factory.annotation.Autowired;import org.springframework.stereotype.Component;import java.util.Date;import java.util.HashMap;import java.util.Map;/** * @author dong */@Componentpublic class JwtTokenUtil {    @Autowired    private JwtProperties jwtProperties;    /**     * 生成token(通过用户名和签名时候用的随机数)     * @param userName     * @param randomKey     * @return     */    public String generateToken(String userName,String randomKey){        Map<String,Object> claims = new HashMap<>();        claims.put(jwtProperties.getMd5Key(),randomKey);        return doGenerateToken(claims,userName);    }    /**     * 生成token     * @param claims     * @param subject     * @return     */    private String doGenerateToken(Map<String,Object> claims,String subject ){        final Date createDate = new Date();        final Date expirationDate = new Date(createDate.getTime()+jwtProperties.getExpiration()*1000);        return Jwts.builder()                .setClaims(claims)                .setSubject(subject)                .setIssuedAt(createDate)                .setExpiration(expirationDate)                .signWith(SignatureAlgorithm.HS512,jwtProperties.getSecret())                .compact();    }    /**     * 获取混淆MD5签名用的随机字符串     * @return     */    public String getRandomKey(){        return RandomUtil.randomString(6);    }    /**     * <pre>     *  验证token是否失效     *  true:过期   false:没过期     * </pre>     */    public Boolean isTokenExpired(String token) {        try {            final Date expiration = getExpirationDateFromToken(token);            return expiration.before(new Date());        } catch (ExpiredJwtException expiredJwtException) {            return true;        }    }    /**     * 获取jwt的payload部分     * @param token     * @return     */    private Claims getClaimFromToken(String token) {        return Jwts.parser()                .setSigningKey(jwtProperties.getSecret())                .parseClaimsJws(token)                .getBody();    }    /**     * 获取用户名从token中     */    public String getUsernameFromToken(String token) {        return getClaimFromToken(token).getSubject();    }    /**     * 获取jwt发布时间     */    public Date getIssuedAtDateFromToken(String token) {        return getClaimFromToken(token).getIssuedAt();    }    /**     * 获取jwt失效时间     */    public Date getExpirationDateFromToken(String token) {        return getClaimFromToken(token).getExpiration();    }    /**     * 解析token是否正确,不正确会报异常<br>     */    public void parseToken(String token) throws JwtException {        Jwts.parser().setSigningKey(jwtProperties.getSecret()).parseClaimsJws(token).getBody();    }    /**     * 获取私有的jwt claim     */    public String getPrivateClaimFromToken(String token, String key) {        return getClaimFromToken(token).get(key).toString();    }    /**     * 获取md5 key从token中     */    public String getMd5KeyFromToken(String token) {        return getPrivateClaimFromToken(token, jwtProperties.getMd5Key());    }}
